5d2adf363baef61c3f92265e3621c1158b716427
rafiqwe <rmlmanik502@gmail.com>
2025-06-07 10:11:32 +0600

    Learn how to create nested route from next.js.Just example is you need about/team . first your create a folder which name is about and than inside the folder create a file which name is page.jsx or .js than agian you create a folder inside the about folder like team  and create a file which is page.jsx if you follow this process than done.Now check it localHost:3000/about/team

---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>Meet the talented individuals behind our company.</p>
+    </div>
+  )
+}
+
+export default page
