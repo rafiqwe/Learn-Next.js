e1802fe810a57dee08242bf0e3dd677227140c5b
rafiqwe <rmlmanik502@gmail.com>
2025-06-07 10:03:31 +0600

    Learn how to create route in next.js.And learn some rules that is first create a folder than create a file and make sure that file name is page.js or page.jsx. if your folder name is just example about and your file name is about.js or .jsx than it is not working. Make sure your file name is page.js or page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1>Hello, Next.js!</h1>
+      <h1>About Us</h1>
+      <p>This is the about page of our Next.js application.</p>
     </div>
   )
 }
--- app/service/page.jsx
+++ app/service/page.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1>Hello, Next.js!</h1>
+      <h1>Our Services</h1>
+      <p>We offer a variety of services to help you succeed.</p>
     </div>
   )
 }
