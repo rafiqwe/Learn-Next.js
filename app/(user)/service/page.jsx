9c53f4088bab28133a4fb7d007a024934596e78a
rafiqwe <rmlmanik502@gmail.com>
2025-09-07 13:38:58 +0600

    Update service page

--- app/(user)/service/page.jsx
+++ app/(user)/service/page.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import Button from "@/Components/Button";
+import React from "react";
+export const metadata = {
+  title: "service Page ",
+  description: "This is service page and check the service ",
+  keywords: ["serivce", "service page", "service check"],
+  authors: [
+    { name: "Muhammad Rabbi" },
+    { name: "Muhammad rabbi", url: "muhammadrabbi-dev.in.com" },
+  ],
+};
 
 const page = () => {
   return (
     <div>
       <h1>Our Services</h1>
       <p>We offer a variety of services to help you succeed.</p>
+      <Button />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
